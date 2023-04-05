aceca38156a021b48a991f697dec6accfa98f7d5
TMarkow <travis.markow@mykenzie.snhu.edu>
2023-04-05 12:58:23 -0500

    Image correction

---
+++ Frontend/src/pages/payscreenPage.js
@@ -0,0 +1,8 @@
+import BaseClass from "../util/baseClass";
+import DataStore from "../util/DataStore";
+import payscreenClient from "../api/payscreenClient";
+
+class PayscreenPage extends BaseClass {
+
+
+}
