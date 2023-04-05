e284a2389fff7e8345519987c89165aa005aefc2
TMarkow <travis.markow@mykenzie.snhu.edu>
2023-04-05 10:38:25 -0500

    Adding missing cancelClient and added to the README

---
+++ Frontend/src/pages/checkoutPage.js
@@ -0,0 +1,13 @@
+import BaseClass from "../util/baseClass";
+import DataStore from "../util/DataStore";
+import checkoutClient from "../api/checkoutClient";
+
+/**
+* Logic needed for the checkout page of the app
+*/
+class Checkout extends BaseClass {
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ATA-Learn-And-Be-Curious-Project
 
 Follow the instructions in the course for completing the group LBC project.
+
+./gradlew :Application:bootRunDev
