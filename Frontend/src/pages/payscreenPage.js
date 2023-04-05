24bfbdf296bc4669118cb121c1abf2f2e59e8ece
TMarkow <travis.markow@mykenzie.snhu.edu>
2023-04-05 15:28:05 -0500

    Beginning payscreenPage.js

--- Frontend/src/pages/payscreenPage.js
+++ Frontend/src/pages/payscreenPage.js
@@ -4,5 +4,28 @@ import payscreenClient from "../api/payscreenClient";
 
 class PayscreenPage extends BaseClass {
 
+    constructor() {
+    super();
+    this.bindClassMethods();
+    this.dataStore = new DataStore;
+    }
+
+    async mount() {
+        document.getElementById('pay_details').addEventListener('click', this.onSubmit);
+        this.client = new payscreenClient();
+
+
+
+    }
+
+
+    //Render Methods-----------------
+
+    async renderPayscreen() {
+        const custName = document.getElementsByName("cust_name")
+
+    }
+
+
 
 }
