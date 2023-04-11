03889c9192113b9196506e1985e2e55c04ffa9af
Victoria Colin <victoria.colin@mykenzie.snhu.edu>
2023-04-11 13:45:01 -0500

    trying to fix rebase

--- Frontend/src/pages/payscreenPage.js
+++ Frontend/src/pages/payscreenPage.js
@@ -2,7 +2,6 @@ import BaseClass from "../util/baseClass";
 import DataStore from "../util/DataStore";
 import PayscreenClient from "../api/payscreenClient";
 
-console.log(document.cookie);
 class PayscreenPage extends BaseClass {
 
 
@@ -32,25 +31,25 @@ class PayscreenPage extends BaseClass {
     }
 
     async submit() {
-        let cookieDelete = 0;
-        document.cookie = cookieDelete;
+        localStorage.removeItem("reservedTicketId");
         window.location ='payment_conf.html';
     }
 
 
     // Throw alert on screen
     async invalidCard() {
-    window.alert("Please enter a valid card number");
+        window.alert("Please enter a valid card number");
     }
 
     // Purchase ticket.
     async validCard() {
-        let ticketId = document.cookie;
+        let ticketId = localStorage.getItem("reservedTicketId");
 
-        if(ticketId == 0) {
+        if(ticketId == null) {
             this.isClosed();
         } else {
             const purchased = await this.client.purchaseTicket(ticketId, 1, this.errorHandler);
+            localStorage.setItem("purchasedTicketId", purchased.ticketId);
             this.submit();
         }
 
